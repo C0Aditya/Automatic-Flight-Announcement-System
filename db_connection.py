20181202dedbd9e92289c5190f8a46c2e8bdf439
C0Aditya <theodricknight0@gmail.com>
2025-07-04 15:12:57 +0530

    Update db_connection.py
    separate_connection_to_db

--- db_connection.py
+++ db_connection.py
@@ -4,6 +4,6 @@ def get_connection():
     return mysql.connector.connect(
         host='localhost',
         user='root',
-        password='ak267120',
-        database='audiofiles'
+        password='',
+        database='name_of_your_database'
     )
